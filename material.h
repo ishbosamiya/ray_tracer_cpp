2c7272cc072486b39b07fa8557a900eea4f6ffe6
Ish Bosamiya <ishbosamiya@gmail.com>
2018-04-28 23:58:14 +0530

    Refactored code to use new material functions for better readability

--- material.h
+++ material.h
@@ -41,7 +41,7 @@ class Metal: public Material {
         Metal(Vec3 albedo, float fuzz) { this->albedo = albedo; this->fuzz = fuzz<0.999?fuzz:0.999;}
 
         virtual bool scatter(Ray &ray_in, Hit_Record &record, Vec3 &attenuation, Ray &scattered) const {
-            Vec3 reflected = reflect(ray_in.directionVector().normalized(), record.normal);
+            Vec3 reflected = reflect(record);
             scattered = Ray(record.point, reflected + (randomInUnitSphere()*fuzz));
             attenuation = albedo;
             return (scattered.directionVector().dot(record.normal) > 0);
@@ -59,27 +59,15 @@ class Dielectric: public Material {
         }
 
         virtual bool scatter(Ray &ray_in, Hit_Record &record, Vec3 &attenuation, Ray &scattered) const {
-            Vec3 outward_normal;
-            Vec3 reflected = reflect(ray_in.directionVector(), record.normal);
+            Vec3 reflected = reflect(record);
             float relative_ri;
             attenuation = Vec3(1.0, 1.0, 1.0);
             Vec3 refracted;
             float reflection_probability;
             float cosine;
 
-            if(ray_in.directionVector().dot(record.normal) > 0) {
-                outward_normal = Vec3(0.0, 0.0, 0.0) - record.normal;
-                relative_ri = refractive_index;
-                cosine = refractive_index * ray_in.directionVector().dot(record.normal) / ray_in.directionVector().length();
-            }
-            else {
-                outward_normal = record.normal;
-                relative_ri = 1.0/refractive_index;
-                cosine = -ray_in.directionVector().dot(record.normal) / ray_in.directionVector().length();
-            }
-
-            if(refract(ray_in.directionVector(), outward_normal, relative_ri, refracted)) {
-                reflection_probability = schlick(cosine, refractive_index);
+            if(refract(record, relative_ri, refracted)) {
+                reflection_probability = fresnelDielectric(refractive_index, record);
             }
             else {
                 reflection_probability = 1.0;
@@ -92,33 +80,6 @@ class Dielectric: public Material {
                 scattered = Ray(record.point, refracted + (randomInUnitSphere()*fuzz));
             }
 
-            return true;
-        }
-};
-
-class Fresnel_Material: public Material {
-    float refractive_index;
-
-    public:
-        Fresnel_Material(float refractive_index) { this->refractive_index = refractive_index;}
-
-        virtual bool scatter(Ray &ray_in, Hit_Record &record, Vec3 &attenuation, Ray &scattered) const {
-            float cosine;
-            float reflection_probability;
-            float relative_ri;
-            if(ray_in.directionVector().dot(record.normal) > 0) {
-                relative_ri = refractive_index;
-                cosine = refractive_index * ray_in.directionVector().dot(record.normal) / ray_in.directionVector().length();
-            }
-            else {
-                relative_ri = 1.0/refractive_index;
-                cosine = -ray_in.directionVector().dot(record.normal) / ray_in.directionVector().length();
-            }
-            reflection_probability = schlick(cosine, relative_ri);
-
-            Vec3 target = record.point + record.normal + randomInUnitSphere();
-            scattered = Ray(record.point, target-record.point);
-            attenuation = Vec3(reflection_probability, reflection_probability, reflection_probability);
             return true;
         }
 };
